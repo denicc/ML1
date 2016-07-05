66762472da2bc42220800a79b265aad7d6e365b1
denicc <denic@outlook.com>
2016-07-05 13:38:58 +0200

    added Stanford NER

--- MediaWikiExtraction/src/stanford/nlp/source/StanfordNER.java
+++ MediaWikiExtraction/src/stanford/nlp/source/StanfordNER.java
@@ -3,6 +3,11 @@ package stanford.nlp.source;
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -53,13 +58,30 @@ public class StanfordNER {
 	 }
 	 public static void main(String args[])
 	 {
-	 String content="Sachin Ramesh Tendulkar (Listeni/ˌsətʃɪn tɛnˈduːlkər/; Marathi: "
-	 + " सचिन रमेश तेंडुलकर; born 24 April 1973) is an Indian former cricketer widely "
-	 + " acknowledged as the greatest batsman of the modern generation, popularly holds the title \"God of Cricket\" among his fans [2] He is also acknowledged as the greatest cricketer of all time.[6][7][8][9] He took up cricket at the age of eleven, made his Test debut against Pakistan at the age of sixteen, and went on to represent Mumbai domestically and India internationally for close to twenty-four years. He is the only player to have scored one hundred international centuries, the first batsman to score a Double Century in a One Day International, and the only player to complete more than 30,000 runs in international cricket.[10] In October 2013, he became the 16th player and first Indian to aggregate "
-	 + " 50,000 runs in all recognized cricket "
-	 + " First-class, List A and Twenty20 combined)";
+		 long startTime = System.currentTimeMillis();
+
+		 try {
+			BufferedReader br = new BufferedReader(new FileReader("testfiles/Organisation-TrainingFile-With-Description.txt"));
+		    StringBuilder sb = new StringBuilder();
+			    String line = br.readLine();
+
+			    while (line != null) {
+			        sb.append(line);
+			        sb.append(System.lineSeparator());
+			        line = br.readLine();
+			        
+			   	 System.out.println(identifyNER(line, "classifiers/english.conll.4class.distsim.crf.ser.gz").toString());
+			   	 
+			    } 
+		 } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	 
-	 System.out.println(identifyNER(content, "classifiers/english.conll.4class.distsim.crf.ser.gz").toString());
+     long stopTime = System.currentTimeMillis();
+     long elapsedTime = stopTime - startTime;
+     System.out.println(elapsedTime);
+     
 	 }
 
 	
